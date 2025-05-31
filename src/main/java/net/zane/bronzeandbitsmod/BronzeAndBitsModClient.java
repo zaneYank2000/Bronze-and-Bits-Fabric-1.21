d2751699b2d018d18e1253c53f1e44ef6220cd86
zan2b <zan2b@hotmail.com>
2025-05-31 15:56:53 -0400

    first

---
+++ src/main/java/net/zane/bronzeandbitsmod/BronzeAndBitsModClient.java
@@ -0,0 +1,11 @@
+package net.zane.bronzeandbitsmod;
+
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BronzeAndBitsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/zane/bronzeandbitsmod/BronzeAndBitsModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.zane.bronzeandbitsmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BronzeAndBitsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
